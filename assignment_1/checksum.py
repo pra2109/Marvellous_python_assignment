7db9e7e918780b8427d04152cf0d1ffb6c51a41d
pra2109 <bhisepp21@gmail.com>
2025-05-10 10:39:55 +0530

    Add files via upload

---
+++ assignment_1/checksum.py
@@ -0,0 +1,12 @@
+def Chksum():
+    no1 = int(input("Enter number:"))
+    if no1 % 2 ==0:
+         print("Number is Even")
+         
+    else:
+        print("Number is Odd")     
+    
+    
+
+if __name__ == "__main__":
+    Chksum()

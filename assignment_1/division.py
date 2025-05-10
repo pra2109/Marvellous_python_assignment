145ab57ee68e5dc25ff4fdcad13f8968473341f8
pra2109 <bhisepp21@gmail.com>
2025-05-10 11:13:03 +0530

    Add files via upload

---
+++ assignment_1/division.py
@@ -0,0 +1,13 @@
+def Chksum():
+    no1 = int(input("Enter number:"))
+    if no1 % 5 ==0:
+         print("True")
+         
+    else:
+        print("False")     
+    
+    
+
+if __name__ == "__main__":
+    Chksum()
+    

fc346cb662fb6c6e8cffaf740af3dc2cde951ff9
pra2109 <bhisepp21@gmail.com>
2025-05-10 10:57:32 +0530

    Add files via upload

---
+++ assignment_1/Add.py
@@ -0,0 +1,15 @@
+def Add():
+    no1 = int(input("Enter first number:"))
+    no2 = int(input("Enter second number:")) 
+    ans = print ("Addtion is :", no1 + no2)
+    
+        
+         
+         
+    
+    
+
+if __name__ == "__main__":
+    Add()
+        
+

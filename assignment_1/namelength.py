efa08ca1de9acb28addedbeb97fad758b80d4dbe
pra2109 <bhisepp21@gmail.com>
2025-05-11 10:07:28 +0530

    Add files via upload

---
+++ assignment_1/namelength.py
@@ -0,0 +1,12 @@
+def main():
+    no1 = str(input("Enter the name:"))
+    #data = input():
+
+    #data["Prasad"]
+    print("lent is :", len(no1))
+    #print("enter")
+
+if __name__ == "__main__":
+    main()
+
+
